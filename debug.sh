770e593b69c290ec96f2e7eef9444b989a79ec4c
Mountain Interval <mtn-interval@users.noreply.github.com>
2024-10-19 11:49:12 +0100

    update

---
+++ debug.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# Automation script by Mountain Interval
+
+# CC_TEXT codes for output
+CC_HEADER='\033[1;35;44m'   # Bold Magenta on Blue background - To mark sections or major steps in the script.
+CC_TEXT='\033[1;34;40m'     # Bold Blue on Black background - For general text, prompts, and success messages.
+CC_RESET='\033[0m'          # Reset CC_TEXT - To reset color coding.
+
+
+
+
+# Function to pause the script
+pause() {
+    sleep 0.2
+}
+
+
+
+
+# Define text separator style
+separator() {
+    echo -e "${CC_TEXT}│${CC_RESET}"
+    pause
+    echo -e "${CC_TEXT}│${CC_RESET}"
+    pause
+    echo -e "${CC_TEXT}│${CC_RESET}"
+    pause
+}
+
+
+
+
+# Script header
+echo -e "${CC_HEADER}────── Debug  v0.01 ──────${CC_RESET}"
+echo
+sleep 1
+
+
+
+
+echo
+echo "Please choose which script to download and run:"
+echo "1) 00--init.sh"
+echo "2) 01--pre.sh"
+echo "3) 02--install.sh"
+echo "4) 03--chroot.sh"
+echo "5) 04--post.sh"
+echo "6) 05--soft.sh"
+
+read -p "Enter the number corresponding to your choice: " choice
+
+case $choice in
+    1)
+        rm -f *--*.sh && wget https://raw.githubusercontent.com/mtn-interval/arch-install-scripts/main/00--init.sh
+        ;;
+    2)
+        rm -f *--*.sh && wget https://raw.githubusercontent.com/mtn-interval/arch-install-scripts/main/01--pre.sh
+        ;;
+    3)
+        rm -f *--*.sh && wget https://raw.githubusercontent.com/mtn-interval/arch-install-scripts/main/02--install.sh
+        ;;
+    4)
+        rm -f *--*.sh && wget https://raw.githubusercontent.com/mtn-interval/arch-install-scripts/main/03--chroot.sh
+        ;;
+    5)
+        rm -f *--*.sh && wget https://raw.githubusercontent.com/mtn-interval/arch-install-scripts/main/04--post.sh
+        ;;
+    6)
+        rm -f *--*.sh && wget https://raw.githubusercontent.com/mtn-interval/arch-install-scripts/main/05--soft.sh
+        ;;
+    *)
+        echo "Invalid option. Exiting..."
+        echo
+        exit 1
+        ;;
+esac
+
